43f9cd1f35925c6c463bbd969576d1ee2eddd2ae
cre01 <cre01>
2017-03-16 15:42:04 +0900

    Create index.html

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ server.listen(process.env.PORT || 3978, function()
    console.log('%s listening to %s', server.name, server.url); 
 });
 
+server.get('/', restify.serveStatic({
+ directory: __dirname,
+ default: '/index.html'
+}));
+
 // Create chat bot
 var connector = new builder.ChatConnector
 ({ appId: 'fc6ddf05-5e04-4c76-8110-c9272305b9bd', appPassword: 'hV0AnPbUHdDkvHLZYxdis3h' }); 
